3af6d2fb1d271eca0f53ca605b8d381efb7c6691
Samuel <cohenzvisamuel@gmail.com>
2023-01-03 00:07:39 +0200

    Added Dash !!!

--- wobble/Animations/MovementView.cs
+++ wobble/Animations/MovementView.cs
@@ -7,8 +7,12 @@ namespace wobble.Animations
 {
     internal class MovementView : SurfaceView, IRunnable
     {
+
+        static int joystickFingerIndex = 0;
+
         int frameWidth;
         int frameHeight;
+
         double angle = 0;
         double distance = 0;
         double actualDistance = 0;
@@ -58,21 +62,45 @@ namespace wobble.Animations
 
         public override bool OnTouchEvent(MotionEvent args)
         {
-            int pointerIndex = args.ActionIndex;
-            MotionEventActions action = args.Action & MotionEventActions.Mask;
-            Point fingerLocation = new Point((int)args.GetX(pointerIndex), (int)args.GetY(pointerIndex));
+            int fingerIndex = args.ActionIndex;
+            Point fingerLocation = new Point((int)args.GetX(fingerIndex), (int)args.GetY(fingerIndex));
+            MotionEventActions action = args.ActionMasked;
 
+            switch (action)
+            {
+                case MotionEventActions.Move:
+                    if (fingerIndex == joystickFingerIndex)
+                    {
+                        HandleJoystickTouch(fingerLocation);
+                    }
+                    break;
+
+                case MotionEventActions.Up:
+                    if (fingerIndex == joystickFingerIndex)
+                    {
+                        this.distance = 0;
+                    }
+                    break;
+
+                case MotionEventActions.Pointer1Down:
+                    HandleDashTouch(fingerLocation);
+                    break;
+            }
+
+            Invalidate();
+            return true;
+        }
+
+        private void HandleJoystickTouch(Point fingerLocation)
+        {
             this.angle = Utils.GetAngleBetweenPoints(joystick.MainLocation, fingerLocation);
             this.actualDistance = Utils.GetDistanceBetweenPoints(joystick.MainLocation, fingerLocation);
             this.distance = Math.Min(actualDistance, Joystick.joystickWorkingRadius);
-
-            if (action == MotionEventActions.Up)
-            {
-                fingerLocation = joystick.MainLocation;
-                this.distance = 0;
-            }
-
-            return true;
+        }
+        private void HandleDashTouch(Point fingerLocation)
+        {
+            this.player.InitDash();
+            System.Console.WriteLine($"pew pew pew!!! ({fingerLocation.X},{fingerLocation.Y})");
         }
     }
 }
--- wobble/Animations/Player.cs
+++ wobble/Animations/Player.cs
@@ -5,7 +5,11 @@ namespace wobble.Animations
 {
     public class Player : ControlledSprite
     {
-        protected override int TopSpeed { get => 30; }
+        private static int DashMaxTimer { get => 7; }
+
+        protected override int TopSpeed { get => 20; }
+        private int DashMultiplier { get => 3; }
+        private int DashTimer { set; get; }
         protected override int Width { get => 100; }
         protected override int Height { get => 100; }
 
@@ -19,25 +23,52 @@ namespace wobble.Animations
 
         public override void CalculateNextPosition(double angle, double distance)
         {
-            double joystickSpeedMultiplier = distance / Joystick.joystickWorkingRadius;
+            double speedMultiplier = CalculateSpeedMultiplier(distance);
+            CalculateNextXLocation(angle, distance, speedMultiplier);
+            CalculateNextYLocation(angle, distance, speedMultiplier);
+            if (DashTimer > 0) DashTimer--;
+        }
 
+        private double CalculateSpeedMultiplier(double distance)
+        {
+            if (DashTimer > 0)
+                return DashMultiplier;
+            else
+                return CalculateJoystickMultiplier(distance);
+        }
+
+        private double CalculateJoystickMultiplier(double distance)
+        {
+            return distance / Joystick.joystickWorkingRadius;
+        }
+
+        private void CalculateNextXLocation(double angle, double distance, double speedMultiplier)
+        {
             xSpeed = Utils.GetXRate(angle);
-            int jumpX = (int)Math.Round(TopSpeed * xSpeed * joystickSpeedMultiplier);
+            int jumpX = (int)Math.Round(TopSpeed * xSpeed * speedMultiplier);
             if (x + jumpX + this.Width >= frameWidth)
                 x = frameWidth - this.Width;
             else if (x + jumpX < 0)
                 x = 0;
             else
                 x += jumpX;
+        }
 
+        private void CalculateNextYLocation(double angle, double distance, double speedMultiplier)
+        {
             ySpeed = Utils.GetYRate(angle);
-            int jumpY = (int)Math.Round(TopSpeed * ySpeed * joystickSpeedMultiplier);
+            int jumpY = (int)Math.Round(TopSpeed * ySpeed * speedMultiplier);
             if (y + jumpY + this.Height >= frameHeight)
                 y = frameHeight - this.Height;
             else if (y + jumpY < 0)
                 y = 0;
             else
                 y += jumpY;
+        }
+
+        public void InitDash()
+        {
+            DashTimer = DashMaxTimer;
         }
     }
 }
